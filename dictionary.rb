0542b0c93ddf65bfc58f6dfdc37363447b137981
dallinforsyth <dallfor192@gmail.com>
2023-01-13 12:47:32 -0600

    created dictionary app, tried working on audio

--- dictionary.rb
+++ dictionary.rb
@@ -8,14 +8,19 @@ while true
   if input_word == "q"
     break
   else
-    response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=200&in")
+    response = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=")
+
+    # response2 = HTTP.get("https://api.wordnik.com/v4/word.json/#{input_word}/audio?useCanonical=false&limit=50&api_key=")
 
     word_data = response.parse(:json)
+    # audio_data = response2o.parse(:json)
 
     word = word_data[0]["word"]
     definition = word_data[1]["text"]
 
     puts "#{input_word}: #{definition}"
+    # import webbrowser
+    # webbrowser.open_new(audio_data[0]["fileUrl"])
     puts " "
   end
 end
